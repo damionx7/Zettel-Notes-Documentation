ca4a5f097423e3ec38d20a3c5b344ecd4ee0117c
Rohit <rohitsharma.royal@gmail.com>
2023-10-02 14:56:36 +0530

    +blue scheme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,27 +31,16 @@ const config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
+  
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+      '@docusaurus/preset-classic',
       ({
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: false,
-        //showReadingTime: true,
-        // Please change this to your repo.
-        // Remove this to remove the "edit this page" links.
-        //editUrl:
-        //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        //,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -76,20 +65,6 @@ const config = {
           alt: 'Zettel Notes Logo',
           src: 'favicon.ico',
         },
-        items: [
-          // {
-          //   type: 'docSidebar',
-          //   sidebarId: 'tutorialSidebar',
-          //   position: 'left',
-          //   label: 'Tutorial',
-          // },
-          // {to: '/blog', label: 'Blog', position: 'left'},
-          // {
-          //   href: 'https://github.com/facebook/docusaurus',
-          //   label: 'GitHub',
-          //   position: 'right',
-          // },
-        ],
       },
       footer: {
         style: 'dark',
